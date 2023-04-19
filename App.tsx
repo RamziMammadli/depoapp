742262cd1e85c5a5c370ae87200bb30383954621
RamziMammadli <remzimemmedli95@gmail.com>
2023-04-19 17:08:35 +0400

    redux-toolkit

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import RootStack from './src/navigation/RootStack'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import RootStack from './src/navigation/RootStack';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootStack/>
-    </NavigationContainer>
-  )
-}
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </Provider>
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/navigation/RootStack.tsx
+++ src/navigation/RootStack.tsx
@@ -1,14 +1,17 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from '../screens/HomeScreen';
+import RootTab from './RootTab';
 
 const Stack = createNativeStackNavigator();
 
 const RootStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen}/>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="RootTab" component={RootTab} />
     </Stack.Navigator>
   );
 };
